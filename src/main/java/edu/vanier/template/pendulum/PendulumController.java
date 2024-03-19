9b39bc98b0ceceb9e626d1828833962edcf971f2
RyanHelou <ryan.helou@gmail.com>
2024-03-19 11:55:11 -0400

    pendulum classes

---
+++ src/main/java/edu/vanier/template/pendulum/PendulumController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.vanier.template.pendulum;
+
+/**
+ *
+ * @author ryanhelou
+ */
+public class PendulumController {
+    private double length1;
+    private double length2;
+    private double mass1;
+    private double mass2;
+    private double angle1;
+    private double angle2;
+    private double velocity1;
+    private double velocity2;
+    
+
+    public PendulumController(double length1, double length2, double mass1, double mass2, double angle1, double angle2, double velocity1, double velocity2) {
+        
+    }
+
+}

b4ab21164f12c3c8097f776f2b0cf87ff72c1234
RyanHelou <ryan.helou@gmail.com>
2024-03-19 12:56:09 -0400

    more

--- src/main/java/edu/vanier/template/pendulum/PendulumController.java
+++ src/main/java/edu/vanier/template/pendulum/PendulumController.java
@@ -17,9 +17,91 @@ public class PendulumController {
     private double angle2;
     private double velocity1;
     private double velocity2;
-    
 
-    public PendulumController(double length1, double length2, double mass1, double mass2, double angle1, double angle2, double velocity1, double velocity2) {
+    public double getLength1() {
+        return length1;
+    }
+
+    public void setLength1(double length1) {
+        this.length1 = length1;
+    }
+
+    public double getLength2() {
+        return length2;
+    }
+
+    public void setLength2(double length2) {
+        this.length2 = length2;
+    }
+
+    public double getMass1() {
+        return mass1;
+    }
+
+    public void setMass1(double mass1) {
+        this.mass1 = mass1;
+    }
+
+    public double getMass2() {
+        return mass2;
+    }
+
+    public void setMass2(double mass2) {
+        this.mass2 = mass2;
+    }
+
+    public double getAngle1() {
+        return angle1;
+    }
+
+    public void setAngle1(double angle1) {
+        this.angle1 = angle1;
+    }
+
+    public double getAngle2() {
+        return angle2;
+    }
+
+    public void setAngle2(double angle2) {
+        this.angle2 = angle2;
+    }
+
+    public double getVelocity1() {
+        return velocity1;
+    }
+
+    public void setVelocity1(double velocity1) {
+        this.velocity1 = velocity1;
+    }
+
+    public double getVelocity2() {
+        return velocity2;
+    }
+
+    public void setVelocity2(double velocity2) {
+        this.velocity2 = velocity2;
+    }
+    
+    
+    public void startAnimation() {
+
+    }
+
+    public void stopAnimation() {
+      
+    }
+
+    public double calculateVerticalComponent() {
+        return 0.0;
+
+    }
+    
+    public double calculateHorizontalComponent() {
+        return 0.0;
+    }
+    
+    
+    public PendulumController(){
         
     }
 

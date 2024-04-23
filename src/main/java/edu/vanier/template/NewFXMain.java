171b62e443728d1228b9a8f104a7765bfa71ec14
sttyh <132113909+sttyhh@users.noreply.github.com>
2024-04-23 13:52:55 -0400

    Working on the main Menu
    minor steps towards creating the main menu

---
+++ src/main/java/edu/vanier/template/NewFXMain.java
@@ -0,0 +1,48 @@
+package edu.vanier.template;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+
+import java.io.IOException;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author 2165566
+ */
+public class NewFXMain extends Application {
+    private Mover mover;
+    private double mouseX;
+    private double mouseY;
+    private boolean showTrail;
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainAnish.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Anish Mehra");
+        primaryStage.setScene(scene);
+        primaryStage.setAlwaysOnTop(true);
+        primaryStage.show();
+        primaryStage.setAlwaysOnTop(false);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ src/main/java/edu/vanier/template/controllers/MainAnishController.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package edu.vanier.template.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author 2165566
+ */
+public class MainAnishController implements Initializable {
+
+    @FXML
+    private ImageView spacebackground;
+    @FXML
+    private ImageView doublependulum;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void dpOnMouseEntered(MouseEvent event) {
+    }
+
+    @FXML
+    private void dpOnMouseClicked(MouseEvent event) {
+    }
+    
+}
